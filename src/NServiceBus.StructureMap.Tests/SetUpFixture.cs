80920b5cc0788bd3ddd90b58c79130df8de63422
Simon Cropp <simon.cropp@gmail.com>
2014-09-17 17:07:00 +1000

    Add unit tests

---
+++ src/NServiceBus.StructureMap.Tests/SetUpFixture.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus.ContainerTests;
+using NUnit.Framework;
+
+[SetUpFixture]
+public class SetUpFixture
+{
+    [SetUp]
+    public void Setup()
+    {
+        TestContainerBuilder.ConstructBuilder = () => new StructureMapObjectBuilder();
+    }
+
+}
