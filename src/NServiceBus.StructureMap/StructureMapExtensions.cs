9937ec4f9721eb75696fef42dd9c9cbff7e6d23a
Andreas Öhlund <andreas.ohlund@particular.net>
2014-09-17 17:37:40 +0200

    Removed unused ext method

--- src/NServiceBus.StructureMap/StructureMapExtensions.cs
+++ src/NServiceBus.StructureMap/StructureMapExtensions.cs
@@ -1,21 +1,8 @@
 using System;
 using StructureMap;
-using StructureMap.Pipeline;
 
 static class StructureMapExtensions
 {
-    /// <summary>
-    /// Registers the given interface and redirects to the given pluginType when the interface is requested
-    /// </summary>
-    public static void RegisterAdditionalInterfaceForPluginType(this ConfigurationExpression configuration, Type implementedInterface, Type pluginType, ILifecycle lifecycle)
-    {
-        var type = typeof(Registration<,>).MakeGenericType(implementedInterface, pluginType);
-
-        var registration = (IRegistration)Activator.CreateInstance(type);
-
-        registration.RegisterServiceInterface(configuration, lifecycle);
-    }
-
     /// <summary>
     /// Tells StructureMap to do setter injection for the given type
     /// </summary>
@@ -23,22 +10,4 @@ static class StructureMapExtensions
     {
         configuration.Policies.SetAllProperties(x =>x.TypeMatches(t=>t == pluginType));
     }
-
-    // The inner type and interface is just a little trick to
-    // grease the generic wheels
-    interface IRegistration
-    {
-        void RegisterServiceInterface(ConfigurationExpression config, ILifecycle callModel);
-    }
-
-    class Registration<TInterface, TImplementor> : IRegistration where TImplementor : TInterface
-    {
-        public void RegisterServiceInterface(ConfigurationExpression config, ILifecycle lifecycle)
-        {
-            config.For<TInterface>()
-                .LifecycleIs(lifecycle)
-                .Use(context => context.GetInstance<TImplementor>());
-        }
-    }
-
 }
