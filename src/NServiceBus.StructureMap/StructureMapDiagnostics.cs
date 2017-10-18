a3c085414d02aa4725f79adfd7a64ff40bdb8b54
Bob Langley <bob.langley@particular.net>
2017-10-18 16:48:41 -0700

    Add diagnostics to report if user is supplying a container

---
+++ src/NServiceBus.StructureMap/StructureMapDiagnostics.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus.Features
+{
+    /// <summary>
+    /// Adds Diagnostics information
+    /// </summary>
+    public class StructureMapDiagnostics : Feature
+    {
+        /// <summary>
+        /// Constructor for diagnostics feature
+        /// </summary>
+        public StructureMapDiagnostics()
+        {
+            EnableByDefault();
+        }
+
+        /// <summary>
+        /// Sets up diagnostics
+        /// </summary>
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Settings.AddStartupDiagnosticsSection("NServiceBus.StructureMap", new
+            {
+                UsingExistingContainer = context.Settings.HasSetting<StructureMapBuilder.ContainerHolder>()
+            });
+        }
+    }
+}
