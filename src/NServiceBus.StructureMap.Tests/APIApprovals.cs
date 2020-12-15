5a655c297a45ee682c96e1288d7e459c2190f4ef
Sean Feldman <feldman.sean@gmail.com>
2020-12-14 21:51:14 -0700

    Fix API Approvals

--- src/NServiceBus.StructureMap.Tests/APIApprovals.cs
+++ src/NServiceBus.StructureMap.Tests/APIApprovals.cs
@@ -9,7 +9,10 @@ public class APIApprovals
     [Test]
     public void Approve()
     {
-        var publicApi = ApiGenerator.GeneratePublicApi(typeof(StructureMapInternalType).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+        var publicApi = typeof(StructureMapInternalType).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+        });
         Approver.Verify(publicApi);
     }
 }
